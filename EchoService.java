253d614cf9fabd3869496e5c6af51e447addecf5
charliembrodie <cbrodi01@cspc061.dcs.bbk.ac.uk>
2012-12-05 15:32:31 +0000

    I

---
+++ EchoService.java
@@ -0,0 +1,14 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+*An implementation of the echo service.
+*/
+public interface EchoService extends Remote{
+	/**
+	*Returns the same string passed as the parameter
+	*@param s a string
+	*@return the same string passed as the parameter.
+	*/
+	public String echo(String s) throws RemoteException;
+}
