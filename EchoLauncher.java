594dc4f923c995ae03ca40c5868d0c7581d4d3bb
charliembrodie <cbrodi01@cspc061.dcs.bbk.ac.uk>
2012-12-05 15:31:13 +0000

    This is one.

---
+++ EchoLauncher.java
@@ -0,0 +1,37 @@
+import java.rmi.*;
+import java.net.*;
+import java.rmi.registry.*;
+
+public class EchoLauncher{
+
+	public static void main(String[] args){
+		EchoLauncher launcher = new EchoLauncher();
+		launcher.launch();
+	}
+	
+	private void launch() {
+
+		// 1. If there is no security manager, start one
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+		
+		try {
+			// 2. Create the registry if there is not one
+			LocateRegistry.createRegistry(1099);
+			// 3. Create the server object
+			EchoServer server = new EchoServer();
+			// 4. Register (bind) the server object on the registy.
+			// The registry may be on a different machine
+			String registryHost = "//localhost/";
+			String serviceName = "echo";
+			Naming.rebind(registryHost + serviceName, server);
+			
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (RemoteException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+}
